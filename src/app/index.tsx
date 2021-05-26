e2a3703177e1f5227e4de72306d6e2039c5acaa4
QuachTrienHuy1107 <quachhuy1107@gmail.com>
2021-05-26 18:55:15 +0700

    setup fake api for comment and food

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -6,22 +6,26 @@
  * contain code that should be seen on all pages. (e.g. navigation bar)
  */
 
+import "antd/dist/antd.css";
 import * as React from "react";
 import { Helmet } from "react-helmet-async";
 import { useTranslation } from "react-i18next";
+import { useDispatch } from "react-redux";
 import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
 import { renderRoutes, routes } from "router";
+import "slick-carousel/slick/slick-theme.css";
+import "slick-carousel/slick/slick.css";
 import GlobalStyle from "styles/global-styles";
 import { ROUTES } from "utils/constants/settings";
 import About from "./pages/About/Loadable";
+import { useAuthSlice } from "./pages/Form/slice";
 import HomePage from "./pages/HomePage/Loadable";
 import { NotFoundPage } from "./pages/NotFoundPage/Loadable";
-import { useDispatch } from "react-redux";
-import { useAuthSlice } from "./pages/Form/slice";
-import { useIdentity } from "hooks/useIdentity";
-import "./app.scss";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+import firebase from "../firebase";
+
+// var database = firebase;
+// console.log("databaseRef", databaseRef);
+// console.log("firebase", firebase.database());
 
 export function App() {
     const { i18n } = useTranslation();
@@ -31,10 +35,27 @@ export function App() {
         const userLogin = localStorage.getItem("user");
         if (userLogin) {
             const credential = JSON.parse(userLogin);
-            console.log("cccc", credential);
+
             disptach(actions.checkLoginActionSuccess(credential));
         }
     }, []);
+
+    /**
+     * Test mode
+     */
+    const [spells, setSpells] = React.useState([]);
+    React.useEffect(() => {
+        const fetchData = async () => {
+            const db = firebase;
+            console.log("db", db);
+            /*   const data = await db.collection("spells").get();
+            setSpells(data.docs.map(doc => ({ ...doc.data(), id: doc.id }))); */
+        };
+        fetchData();
+    }, []);
+
+    //=================
+
     return (
         <BrowserRouter>
             <Helmet
